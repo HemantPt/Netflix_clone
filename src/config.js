e4b7c2bde9c447b479f34375a9316bec95122c42
HemantPt <hp160203@gmail.com>
2025-02-08 22:10:08 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,16 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCqFhPwNPO_resm3euKC8DSHVTLf7Z0xjM",
+  authDomain: "netflix-clone-8b636.firebaseapp.com",
+  projectId: "netflix-clone-8b636",
+  storageBucket: "netflix-clone-8b636.firebasestorage.app",
+  messagingSenderId: "731329826194",
+  appId: "1:731329826194:web:8b9f1ab920155a69a1854b"
+};
+
+
+
+const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Y2JkMjg4ODUyMDU5MzgyMGVjM2M1NTg5Y2ViNjM0OSIsIm5iZiI6MTczODgyNzIxOC4wNTIsInN1YiI6IjY3YTQ2NWQyNzdiOGNlZDQ1NjY2ZDlkZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hIBpuqNperdXh1OH8OiTO8ZCb8oYf6cBtV6qhTtAanw";
+
+
+  export {firebaseConfig, TMDB_Access_Key};
